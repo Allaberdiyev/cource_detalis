78a70fbe91ef7d6cb395f373a41781c58737ece6
Allaberdiyev <149064535+Allaberdiyev@users.noreply.github.com>
2024-05-11 03:52:05 +0500

    build

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:course_ui/screens/homework_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CourseApp());
+}
+
+class CourseApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return HomeworkScreen();
+  }
+}
